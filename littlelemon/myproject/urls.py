c0369c2f9c0952f06e46e4efc19a7e49201e0571
Reuven Taplashvili <91845041+RUBIKX107@users.noreply.github.com>
2025-07-16 16:57:14 +0000

    new files added

--- littlelemon/myproject/urls.py
+++ littlelemon/myproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
+from restaurant.views import booking_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('bookings/', booking_view, name='booking'),
 ]
